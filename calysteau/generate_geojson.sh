d91a9939864f6d123e97aacbb791d22c0242aef5
Calysteau <91131962+calysteau@users.noreply.github.com>
2022-05-11 11:17:59 +0200

    Create generate_geojson.sh
    Generate GEOJSON files for each SHAPEFILE in downloaded archive

---
+++ calysteau/generate_geojson.sh
@@ -0,0 +1,15 @@
+#/!bin/sh
+ARCHIVE_NAME=$1
+
+SHAPES=`find dataset/${ARCHIVE_NAME} -name *.shp`
+WORKDIR=`pwd`
+for SHAPE in $SHAPES; do
+  SHAPE_PATH=`dirname "${SHAPE}"`
+  SHAPE_BASENAME=`basename "${SHAPE}"`
+  JSON_BASENAME=`echo $SHAPE_BASENAME | sed -e 's/.shp/.geojson/g'`
+  JSON_BASENAME=`echo "$JSON_BASENAME" | tr '[:upper:]' '[:lower:]'`
+  echo "Converting $SHAPE_BASENAME in $JSON_BASENAME"
+  cd $SHAPE_PATH
+  mapshaper -i $SHAPE_BASENAME -o format=geojson precision=0.000001 $JSON_BASENAME
+  cd $WORKDIR
+done
