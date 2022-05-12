dc06c0112d5d72430a7dfadcc911749da6cdb473
Calysteau <91131962+calysteau@users.noreply.github.com>
2022-05-12 13:34:26 +0200

    Update generate_geojsons.sh

--- calysteau/generate_geojsons.sh
+++ calysteau/generate_geojsons.sh
@@ -10,6 +10,6 @@ for SHAPE in $SHAPES; do
   JSON_BASENAME=`echo "$JSON_BASENAME" | tr '[:upper:]' '[:lower:]'`
   echo "Converting $SHAPE_BASENAME in $JSON_BASENAME"
   cd $SHAPE_PATH
-  mapshaper -i $SHAPE_BASENAME -o format=geojson precision=0.000001 $JSON_BASENAME
+  mapshaper -i $SHAPE_BASENAME -proj wgs84 -o format=geojson precision=0.000001 $JSON_BASENAME
   cd $WORKDIR
 done
