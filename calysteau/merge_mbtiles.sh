7468f3cdd5f45703e4aa8ea2805fd4101611ed03
Calysteau <91131962+calysteau@users.noreply.github.com>
2022-05-19 09:47:19 +0200

    Update merge_mbtiles.sh

--- calysteau/merge_mbtiles.sh
+++ calysteau/merge_mbtiles.sh
@@ -4,4 +4,4 @@ DATASET_NAME=$1
 MBTILES=`find dataset/${DATASET_NAME} -name '*.mbtiles'`
 
 echo "Merging $DATASET_NAME mbtiles"
-tile-join -f -o dataset/$DATASET_NAME.mbtiles $MBTILES
+tile-join -pk -f -o dataset/$DATASET_NAME.mbtiles $MBTILES
